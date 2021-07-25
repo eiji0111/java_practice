85a90abe079d7880475f59aa8e96cd0e3ddac0b9
eiji0111 <ee111fender@gmail.com>
2021-07-25 18:17:32 +0900

    [Finish]lesson13

---
+++ Lesson13/Bank.java
@@ -0,0 +1,24 @@
+package Lesson13;
+
+/**
+ * 銀行クラス。
+ * このクラスは、１つの銀行を表します。
+ * @author 森
+ * @see Owner
+ * @since ver1.0.0
+ */
+public class Bank {
+  /** 銀行名 */
+  String name;
+  /** 銀行の住所 */
+  String address;
+  /**
+   * 銀行口座を開設するメソッド。
+   * @param owner 開設者
+   * @param initialBalance 開設時残高
+   */
+  public void addAccount(String owner, int initialBalance) { }
+  public static void main(String[] args) {
+    System.out.println("試験用のメインメソッドです");
+  }
+}
